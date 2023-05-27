ac7fcbcd6725e0baaf186abb6bdcdeb445a56b48
Justin Carville <jccarville@gmail.com>
2023-05-27 22:51:51 +0900

    conditions and visuals for neck, hands, hips, knees all coded and working

--- proposture/metrics.py
+++ proposture/metrics.py
@@ -14,7 +14,7 @@ def get_reps_and_stage(elbow_angles, rep_counter, stage):
         rep_counter +=1
     return stage, rep_counter
 
-# Uses angles to write posture conditional statements
+# NECK conditional statements
 def get_neck(neck_angles, sideview_angle, advice_neck = None):
     left_neck_angle, right_neck_angle = neck_angles
     if sideview_angle == 'left':
@@ -35,23 +35,65 @@ def get_neck(neck_angles, sideview_angle, advice_neck = None):
             advice_neck = 'Tuck chin in'
     return neck_status_color, advice_neck
 
-# Uses angles to write posture conditional statements
-# def get_hips(hip_angles, sideview_angle, advice_hip = None):
-#     left_hip_angle, right_hip_angle = hip_angles
-#     if sideview_angle == 'left':
-#         if left_hip_angle >= 165:
-#             hip_status_color= (31, 194, 53)
-#         if left_hip_angle in range(150,165):
-#             neck_status_color= (0, 255, 255)
-#         if left_hip_angle < 150:
-#             hip_status_color= (14, 14, 232)
-#             advice_hip = 'Tuck chin in'
-#     if sideview_angle == 'right':
-#         if right_hip_angle >= 165:
-#             hip_status_color= (31, 194, 53)
-#         if right_hip_angle in range(150,165):
-#             hip_status_color= (0, 255, 255)
-#         if right_hip_angle < 150:
-#             hip_status_color = (14, 14, 232)
-#             advice_hip = 'Tuck chin in'
-#     return hip_status_color, advice_hip
+# TODO: this condition doesn't work well
+# HAND conditional statements
+def get_hand(shoulder_distance, elbow_angles, sideview_angle, hand_status_color = (31, 194, 53), advice_hand = None):
+    left_x_distance, right_x_distance = shoulder_distance
+    left_elbow_angle, right_elbow_angle = elbow_angles
+    if sideview_angle == "left":
+        if left_elbow_angle > 150:
+            if left_x_distance < 0.05:
+                hand_status_color = (31, 194, 53)
+            if left_x_distance > 0.05:
+                hand_status_color = (14, 14, 232)
+                advice_hand = 'Align hands with shoulders'
+    if sideview_angle == "right":
+        if right_elbow_angle > 150:
+            if right_x_distance < 0.05:
+                hand_status_color = (31, 194, 53)
+            if right_x_distance > 0.05:
+                hand_status_color = (14, 14, 232)
+                advice_hand = 'Align hands with shoulders'
+    return hand_status_color, advice_hand
+
+# HIP conditional statements
+def get_hip(hip_angles, sideview_angle, advice_hip = None):
+    left_hip_angle, right_hip_angle = hip_angles
+    if sideview_angle == 'left':
+        if left_hip_angle >= 170:
+            hip_status_color= (31, 194, 53)
+        if left_hip_angle in range(160,170):
+            hip_status_color= (0, 255, 255)
+        if left_hip_angle < 160:
+            hip_status_color= (14, 14, 232)
+            advice_hip = 'Lower hips'
+    if sideview_angle == 'right':
+        if right_hip_angle >= 170:
+            hip_status_color= (31, 194, 53)
+        if right_hip_angle in range(160,170):
+            hip_status_color= (0, 255, 255)
+        if right_hip_angle < 160:
+            hip_status_color = (14, 14, 232)
+            advice_hip = 'Lower hips'
+    return hip_status_color, advice_hip
+
+# KNEE conditional statements
+def get_knee(knee_angles, sideview_angle, advice_knee = None):
+    left_knee_angle, right_knee_angle = knee_angles
+    if sideview_angle == 'left':
+        if left_knee_angle >= 160:
+            knee_status_color= (31, 194, 53)
+        if left_knee_angle in range(155,160):
+            knee_status_color= (0, 255, 255)
+        if left_knee_angle < 155:
+            knee_status_color= (14, 14, 232)
+            advice_knee = 'Straighten legs'
+    if sideview_angle == 'right':
+        if right_knee_angle >= 160:
+            knee_status_color= (31, 194, 53)
+        if right_knee_angle in range(155,160):
+            knee_status_color= (0, 255, 255)
+        if right_knee_angle < 155:
+            knee_status_color = (14, 14, 232)
+            advice_knee = 'Straighten legs'
+    return knee_status_color, advice_knee
