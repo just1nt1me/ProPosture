14935173c5cba027fc522a827f863ff381a243da
Justin Carville <jccarville@gmail.com>
2023-07-03 17:26:28 +0900

    updated flask app, created new main function to output frames for flask app

--- proposture/app.py
+++ proposture/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
-from proposture.utils import load_video, get_video_dimensions
-from proposture.main import main  # Import the main function from your app.py
+#import libraries
+import cv2
+import mediapipe as mp
+from flask import Flask, render_template, request, Response
+
+from proposture.new_main import main  # Import the main function from your app.py
 # Import other necessary modules and functions as needed
+from proposture.utils import load_video, get_angles, get_landmarks, get_video_dimensions, get_sideview
+from proposture.metrics import get_reps_and_stage, get_rep_advice, get_neck, get_hip, get_knee, get_hand, get_hand_align, get_shoulder_elbow_dist
+from proposture.visuals import show_status, show_neck, show_hip, show_knee, show_hand, show_align, show_elbow
+from proposture.report import get_full_reps, get_pdf
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
 app = Flask(__name__)
-# ... your existing code ...
+
 
 '''TODO: main page explains ProPosture
 then allow user to select from FRONTVIEW LIVE FEED
@@ -34,17 +44,22 @@ def upload():
     if video.filename == '':
         return 'No video selected'
     if video and allowed_file(video.filename):
-        video.save('static/videos/' + video.filename)
-        return render_template('sideview.html', video_name=video.filename)
+        video_file_path = 'static/videos/' + video.filename
+        video.save(video_file_path)
+        cap = load_video(video_file_path)
+        height, width = get_video_dimensions(cap)
+        advice_list = []
+        return Response(main(cap, height, width, view='side'),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
     return 'Invalid video file'
 
-@app.route('/video')
-def video():
-    # Call the main function or any other functions from your app.py
-    cap = load_video("media/latest_sideview.mp4")
+@app.route('/livestream')
+def livestream():
+    cap = cv2.VideoCapture(0)
     height, width = get_video_dimensions(cap)
-    main(cap, height, width, view='side')
-    return "Video processing completed"
+    advice_list = []
+    return Response(main(cap, height, width, view='front'),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
